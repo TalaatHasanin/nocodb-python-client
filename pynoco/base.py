a430ce00f4d8a3a70871901b1c236b6d1718502d
TalaatHasanin <talaat049@gmail.com>
2024-09-26 17:57:25 +0300

    add base methods

--- pynoco/base.py
+++ pynoco/base.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from pynoco.source import Source
 
 
@@ -5,6 +7,7 @@ class Base:
     """
     NocoDB base
     """
+
     def __init__(
             self,
             client,
@@ -16,6 +19,7 @@ class Base:
             status: str = None,
             **kwargs
     ):
+        self.api = client.api
         self.client = client
         self.id = id
         self.title = title
@@ -25,10 +29,25 @@ class Base:
         self.kwargs = kwargs
 
         self.sources = []
-        if sources is not None:
-            for source in sources:
-                src = Source(**source)
-                self.sources.append(src)
+
+        for source in sources:
+            src = Source(**source)
+            self.sources.append(src)
+
+    def info(self):
+        data = self.api.get(f'/meta/bases/{self.id}/info')
+        return data.json()
+
+    def delete_source(self, source: Union[str, Source]):
+        if len(self.sources) > 1:
+            if isinstance(source, str):
+                self.api.delete(f'/meta/bases/{self.id}/sources/{source}')
+            elif isinstance(source, Source):
+                self.api.delete(f'/meta/bases/{self.id}/sources/{source.id}')
+            else:
+                raise ValueError(f'source must be either a string of source id or a Source object')
+        else:
+            raise Exception(f'There are no sources connected to {self.title}')
 
 
 class Bases:
@@ -36,7 +55,45 @@ class Bases:
         self.api = client.api
         self.client = client
 
-    def get(self, base_id: str):
+    def get(self, base_id: str) -> Base:
         item = self.api.get(f'/meta/bases/{base_id}').json()
         return Base(self.client, **item)
 
+    def list(self) -> List[Base]:
+        data = self.api.get(f'/meta/bases').json()
+        bases_list = []
+        for base in data['list']:
+            bases_list.append(Base(self.client, **base))
+        return bases_list
+
+    def create(
+            self,
+            base_name: str,
+            sources=None,
+            type: str = 'database',
+            **kwargs
+    ) -> Base:
+        sources_list = []
+        if sources:
+            if sources is List[Source]:
+                for src in sources:
+                    source = src.__dict__
+                    source = source.update(source.pop('kwargs'))
+                    sources_list.append(source)
+            else:
+                sources_list = sources
+
+        base = self.api.post(
+            f'/meta/bases',
+            data={
+                'title': base_name,
+                'sources': sources_list,
+                'type': type,
+                **kwargs
+            }
+        ).json()
+
+        return self.get(base['id'])
+
+    def drop(self, base_id: str) -> None:
+        self.api.delete(f'/meta/bases/{base_id}')
